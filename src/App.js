afc41dcff3ea04248213746233ab3a530d7b29c4
Amber-Pittman <amber@amberpittman.com>
2019-10-02 20:30:08 -0400

    Imported files into App. Made Todo data info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import ReactDom from "react-dom";
+import Todo from "./components/TodoComponents/Todo";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, and any change handlers you need to work with 
+  // your state
+  constructor() {
+    super()
+
+    this.state = {
+      todo: Todo // uses Todo.js file here
+    }
+  }
+
   render() {
     return (
       <div>
---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,33 @@
+export default [
+    {
+      name: "Get Kids to School",
+      id: 100,
+      purchased: false
+    },
+    {
+      name: "Daily Standup at Work",
+      id: 200,
+      purchased: false
+    },
+    {
+      name: "Touch Base with Clients",
+      id: 300,
+      purchased: false
+    },
+    {
+      name: "Follow up with Lead on Bugs",
+      id: 400,
+      purchased: false
+    },
+    {
+      name: "Lunch with Mentor",
+      id: 500,
+      purchased: false
+    },
+    {
+      name: "Research New React Libraries",
+      id: 600,
+      purchased: false
+    }
+  ];
+  
